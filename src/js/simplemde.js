ba290953df2f270c71e69473bf382a624f3c2b7a
syshen <sysheen@gmail.com>
2018-03-10 22:02:10 +0800

    hide-token

--- src/js/simplemde.js
+++ src/js/simplemde.js
@@ -8,7 +8,7 @@ require("codemirror/mode/markdown/markdown.js");
 require("codemirror/addon/mode/overlay.js");
 require("codemirror/addon/display/placeholder.js");
 require("codemirror/addon/selection/mark-selection.js");
-require("./codemirror/hide-token.js");
+require("codemirror/addon/hide/hide-token.js");
 require("codemirror/mode/gfm/gfm.js");
 require("codemirror/mode/xml/xml.js");
 var CodeMirrorSpellChecker = require("codemirror-spell-checker");
